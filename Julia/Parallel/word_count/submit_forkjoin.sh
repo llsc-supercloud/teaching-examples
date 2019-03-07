ca85a8ab8e7d3bb10791fbb87e88ade852eb37d4
Lauren Milechin <lauren.milechin@mit.edu>
2019-03-07 16:22:36 -0500

    adding Julia Parallel word count example

---
+++ Julia/Parallel/word_count/submit_forkjoin.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Slurm sbatch options
+#SBATCH -o top5norm_forkjoin.log-%j
+#SBATCH -n 14
+
+
+# Initialize Modules
+source /etc/profile
+
+# Load Julia Module
+module load julia-1.0
+
+# Call your script as you would from the command line
+julia top5norm_forkjoin.jl
---
+++ Julia/Parallel/word_count/top5norm_forkjoin.jl
@@ -0,0 +1,42 @@
+# Activate environment
+
+using ClusterManagers,Distributed
+
+# Add in the cores allocated by the scheduler as workers
+addprocs(SlurmManager(parse(Int,ENV["SLURM_NTASKS"])-1))
+print("Added workers: ")
+println(nworkers())
+
+# Load required packages and helpers on all processes
+@everywhere begin
+    using DistributedArrays,Statistics
+    include("parallelhelpers.jl")
+end
+
+# Load the file names
+dataLoc = "../../../data/word_count/";
+fnames = dataLoc.*readdir(dataLoc)
+
+# Create a distributed array with the file names
+dfnames = distribute(fnames)
+
+# Calculate the word counts
+# Broadcast over a distributed array automatically does the operation on the correct worker
+allcounts =  getcounts.(dfnames)
+
+# Collect the distributed array onto the leader process
+allcountsL = convert(Array,allcounts)
+
+# Calculate the overall counts
+overallcounts = getoverallcount(allcountsL)
+
+# Share the overall counts with each of the workers
+@everywhere overallcounts=$overallcounts
+
+# Calculate the normalized counts on each worker
+normcount = getnormcount.(allcounts)
+
+# Print out the top 5 words for each file and their counts on the leader process
+for i=1:length(normcount)
+    println(sort(collect(normcount[i]), by=x->x[2], rev=true)[1:min(5,length(normcount[i])),:])
+end
