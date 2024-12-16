5d605022e08733e8e16ecb0416a3ae3305140daf
Lauren Milechin <milechin@mit.edu>
2024-12-16 17:10:27 -0500

    updating distributed julia wc examples

--- Julia/word_count/Parallel/distributed_arrays/submit.sh
+++ Julia/word_count/Parallel/distributed_arrays/submit.sh
@@ -5,7 +5,7 @@
 #SBATCH -n 4
 
 # Load Julia Module
-module load julia/1.7.3
+module load julia/1.9.1
 
 # Call your script as you would from the command line
-julia top5norm_forkjoin.jl
+julia top5norm.jl
