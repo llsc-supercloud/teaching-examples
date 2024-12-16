add024175884e04ac565525974595513467a2e8d
Lauren Milechin <milechin@mit.edu>
2024-12-16 14:10:38 -0500

    separate c7 jobscript for mpi4py example

--- Python/word_count/Parallel/mpi4py/submit_centos7.sh
+++ Python/word_count/Parallel/mpi4py/submit_centos7.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # Slurm sbatch options
-#SBATCH -o top5norm_MapReduce.log-%j
+#SBATCH -o top5norm_forkjoin.log-%j
 #SBATCH -n 4
 
 # Load Anaconda and MPI module
+module load gcc/9.3.0
+module load openmpi/4.0.5
+
 module load miniforge
-module load openmpi
+
 
 # Call your script as you would from the command line
-mpirun python top5norm_MapReduce.py
+mpirun python top5norm_forkjoin.py
+
+
